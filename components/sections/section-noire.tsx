afa44f937b22048879e91e9ee1d116d984ca0cbe
TomDelavega <altommasson@yahoo.fr>
2024-10-02 19:49:13 +0200

    Ajout de l'ombre sur la section noire

--- components/sections/section-noire.tsx
+++ components/sections/section-noire.tsx
@@ -3,15 +3,15 @@ import styles from "./section-noire.module.scss";
 
 const BlackSection = ({ children, titre, description }: ISection) => {
 	return (
-		<>
-			<div className={styles.background}>
+		<div className={styles.background}>
+			<div className={styles.clipPath}>
 				<div className={styles.centered}>
 					<Section titre={titre} description={description}>
 						{children}
 					</Section>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
