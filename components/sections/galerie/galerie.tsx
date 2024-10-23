9de631729d60a7a45fde44ba7f591cf929ddbccd
TomDelavega <altommasson@yahoo.fr>
2024-10-23 09:27:34 +0200

    Implémentation de la version téléphone

--- components/sections/galerie/galerie.tsx
+++ components/sections/galerie/galerie.tsx
@@ -2,7 +2,7 @@ import EmblaCarousel from "./EmblaCarousel";
 import { EmblaOptionsType } from "embla-carousel";
 
 const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true };
-const SLIDE_COUNT = 15;
+const SLIDE_COUNT = 9;
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 export default function Galerie() {
